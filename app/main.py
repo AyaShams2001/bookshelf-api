44b12165f3ba06bf99b081a786c02ba209a4c48b
AyaShams2001 <ayashams42@yahoo.com>
2026-04-16 17:27:13 +0300

    Remove startup seed data

--- app/main.py
+++ app/main.py
@@ -1,38 +1,9 @@
-
 from fastapi import FastAPI
-from .routes import router
-from .db_models import BookTable
 from .database import engine, Base, SessionLocal
+from .routes import router
 
 app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
 
 app.include_router(router)
-
-
-@app.get("/")
-def home():
-    return {"message": "Bookshelf API"}
-
-
-
-def seed_data():
-    db = SessionLocal()
-
-    if db.query(BookTable).count() == 0:
-        books = [
-            BookTable(title="Atomic Habits", author="James Clear", year=2018, rating=4.5),
-            BookTable(title="Sapiens", author="Yuval Noah Harari", year=2011, rating=4.8),
-            BookTable(title="The Alchemist", author="Paulo Coelho", year=1988, rating=4.2),
-        ]
-
-        db.add_all(books)
-        db.commit()
-
-    db.close()
-
-
-@app.on_event("startup")
-def startup_event():
-    seed_data()    
